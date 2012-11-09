3b439e0421dd4c00143305f62deee9590b27f811
Chris van Es <cvanes@gmail.com>
2012-11-09 23:53:10 +0000

    adding keyboard support for down

--- tetris.rb
+++ tetris.rb
@@ -1,10 +1,19 @@
-$board_x_size = 10
-$board_y_size = 15
+require 'curses'
+
+BOARD_X_SIZE = 20
+BOARD_Y_SIZE = 30
+STATUS_COLUMN = 22
+
+def write(row = 0, column = 0, text)
+  Curses.setpos(row, column)
+  Curses.addstr(text);
+  Curses.refresh
+end
 
 class Tetris
   def initialize
     @pieces = ["I", "J", "L", "O", "S", "Z", "T"]
-    @board = Array.new($board_x_size) {Array.new($board_y_size, "-")}
+    @board = Array.new(BOARD_X_SIZE) {Array.new(BOARD_Y_SIZE, "-")}
     @level = 1
   end
 
@@ -13,43 +22,59 @@ class Tetris
   end
 
   def draw
-    puts "\n"
+    game_board = "\n"
     @board.each do |row|
       row.each do |column|
-        print column
+        game_board += column
       end
-      print "\n"
+      game_board += "\n"
     end
+    write(game_board)
+  end
+
+  def mark_active_piece_inactive
+    clear_board_of_active_pieces "o"
   end
 
   def clear_board_of_active_pieces(newCharacter)
     iterate_over_board { |row,column| if @board[row][column] == "x"; @board[row][column] = newCharacter end }
   end
 
-  def move_active_piece_one_space_in_y_dimension
-    @active_piece.row += 1
+  def rotate
 
-    active_piece_row = @active_piece.row
+  end
+
+  def left
+
+  end
+
+  def right
+
+  end
+
+  def down
     if @active_piece.hit_bottom?
-      clear_board_of_active_pieces "o"
+      mark_active_piece_inactive
       newPiece
     else
       clear_board_of_active_pieces "-"
       # move to new position
-      iterate_over_active_piece { |row,column| @board[row + active_piece_row][column] = @active_piece.blocks[row][column] }
+      iterate_over_active_piece { |row,column| @board[row + @active_piece.row][column] = @active_piece.blocks[row][column] }
+      @active_piece.row += 1
       if next_move_blocked_vertically?
-        clear_board_of_active_pieces "o"
+        mark_active_piece_inactive
         newPiece
       end
     end
+    draw
   end
 
   def game_over?
-    next_move_blocked_vertically? && @active_piece.row <= 0
+    next_move_blocked_vertically? && @active_piece.row == 0
   end
 
   def next_move_blocked_vertically?
-    if @active_piece.bottom >= $board_x_size
+    if @active_piece.bottom >= BOARD_X_SIZE
       return true
     end
     for i in 0..@active_piece.blocks[@active_piece.height - 1].length
@@ -58,11 +83,6 @@ class Tetris
       end
     end
     false
-  end
-
-  def move_active_piece
-    move_active_piece_one_space_in_y_dimension
-    draw
   end
 
   def iterate_over_board
@@ -82,14 +102,37 @@ class Tetris
   end
 
   def start_game
-    newPiece
-    loop do
-      move_active_piece
-      sleep 1 / @level
-      if game_over?
-        puts "Game Over!!!"
-        break
+    Thread.new {
+      loop do
+        handle_user_input
       end
+    }
+
+    Thread.new {
+      newPiece
+      loop do
+        down
+        sleep 1 / @level
+        if game_over?
+          write(STATUS_COLUMN, 0, "Game Over!!!\n\nPress any key...")
+          Curses.timeout = -1
+          Curses.getch
+          break
+        end
+      end
+    }.join
+  end
+
+  def handle_user_input
+    case Curses.getch
+      when Curses::Key::UP
+        rotate
+      when Curses::Key::DOWN
+        down
+      when Curses::Key::LEFT
+        left
+      when Curses::Key::RIGHT
+        right
     end
   end
 end
@@ -99,8 +142,8 @@ class Tetrimino
   attr_accessor :row, :column
 
   def initialize
-    @column = -1
-    @row = -1
+    @column = 0
+    @row = 0
   end
 
   def height
@@ -108,11 +151,11 @@ class Tetrimino
   end
 
   def bottom
-    @row + height
+    @row + height - 1
   end
 
   def hit_bottom?
-    @row + @blocks.length > $board_x_size
+    bottom >= BOARD_X_SIZE
   end
 end
 
@@ -167,4 +210,10 @@ class T < Tetrimino
   end
 end
 
+Curses.noecho
+Curses.init_screen
+Curses.stdscr.keypad(true)
+
 Tetris.new.start_game
+
+Curses.close_screen
